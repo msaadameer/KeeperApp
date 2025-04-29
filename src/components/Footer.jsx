cf4baec56ac02ddaa067b82815c1192d05a357b2
Saad Ameer <msaadameer@gmail.com>
2025-04-29 16:23:13 +0500

    ui changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer>
-      <p>Copyright ⓒ {year}</p>
+    <footer className="custom-footer">
+      <p>
+        <FontAwesomeIcon icon={faCopyright} className="footer-icon" /> {year} My Notes App
+      </p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStickyNote } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
-    <header>
-      <h1>Notes</h1>
+    <header className="custom-header">
+      <FontAwesomeIcon icon={faStickyNote} className="header-icon" />
+      <h1 className="header-title">My Notes</h1>
     </header>
   );
 }
